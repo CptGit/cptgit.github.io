ac9700f5246581afbf48fb068ea0953dfab6d4b4
Zhiqiang Zang <capapoc@gmail.com>
2019-08-07 15:29:28 +0800

    Updated scripts to support install only if not exists and so on.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+## Get the current path of the script
 _DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+## Get the path of project root directory
 ROOT_DIR="${_DIR}/.."
 
-(
-        cd "${ROOT_DIR}"
+## Changed the current directory to the project root directory
+pushd "${ROOT_DIR}" > /dev/null
 
-        sudo apt install ruby-full build-essential zlib1g-dev
+## Install dependent packages on Ubuntu if they don't exist
+declare -a PKGS=("ruby-full" "build-essential" "zlib1g-dev")
+for pkg in "${PKGS[@]}"; do
+        dpkg -s "${pkg}" > /dev/null || sudo apt install "${pkg}"
+done
 
-        ## Uncomment the following line if you are behind the GFW
-        # gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
-        gem install bundler -v 2.0.2
+## Uncomment the following line if you are behind the GFW
+# gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
+## Install bundler
+gem install bundler -v 2.0.2
 
-        BUNDLE_PATH=$( gem env | grep 'EXECUTABLE DIRECTORY' | sed 's/[^/]*\//\//' | sed 's/[ \t]*$//' )
-        export PATH="${BUNDLE_PATH}:${PATH}"
+## Add bundle binary executable to PATH (onetime)
+BUNDLE_PATH=$( gem env | grep 'EXECUTABLE DIRECTORY' | sed 's/[^/]*\//\//' | sed 's/[ \t]*$//' )
+export PATH="${BUNDLE_PATH}:${PATH}"
 
-        ## Uncomment the following line if you are behind the GFW
-        # bundle config mirror.https://rubygems.org https://gems.ruby-china.com
+## Uncomment the following line if you are behind the GFW
+# bundle config mirror.https://rubygems.org https://gems.ruby-china.com
+## Install dependent gems for the project
+bundle install --path vendor/bundle
 
-        bundle install --path vendor/bundle
-)
+## Go back to the original directory
+popd > /dev/null
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+## Get the current path of the script
 _DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+## Get the path of project root directory
 ROOT_DIR="${_DIR}/.."
 
-(
-        cd "${ROOT_DIR}"
-        bundle exec jekyll serve
-)
+## Changed the current directory to the project root directory
+pushd "${ROOT_DIR}" > /dev/null
+
+## Start a local server
+bundle exec jekyll serve
+
+## Go back to the original directory
+popd > /dev/null
