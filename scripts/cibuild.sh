104d41e96f3d20a7c7ad97c89b80e22d7c044aff
Zhiqiang Zang <zhiqiang.zang@utexas.edu>
2025-06-21 00:48:48 -0700

    fix: exclude /www.bupt.edu.cn/ in htmlproofer check in CI build script.

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer --ignore-urls '/www.flaticon.com/,/www.linkedin.com/,/www.freepik.com/' ./_site
+bundle exec htmlproofer --ignore-urls '/www.flaticon.com/,/www.linkedin.com/,/www.freepik.com/,/www.bupt.edu.cn/' ./_site
